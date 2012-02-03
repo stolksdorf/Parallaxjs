1fb55c27a4761efaa5be2bc1e995a0bfef4b3526
Scott <scott.tolksdorf@gmail.com>
2012-02-03 17:36:52 -0500

    Updated the demo page with the new features

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+var rightKey, leftKey, topKey, bottomKey;
 
 
 $(document).ready(function () {
+	
+	//Set up the triggers for the arrow keys
+	$(document).keydown(function(e){
+		if (e.keyCode == 37 && typeof leftKey === 'function') { 
+			leftKey();
+		} else if(e.keyCode == 38 && typeof topKey === 'function') { 
+			topKey();
+		} else if(e.keyCode == 39 && typeof rightKey === 'function') { 
+			rightKey();
+		} else if(e.keyCode == 40 && typeof bottomKey === 'function') { 
+			bottomKey();
+		}
+	});
 	
 	
 	parallax.add($("#features"))
@@ -11,71 +25,75 @@ $(document).ready(function () {
 			
 	parallax.background = $("body");
 	
-	
-	parallax.index.onload=function(){
-		
-		$("#rightText").text("Features");
-		$("#rightControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.features.right();
-		});
-		
-		$("#topText").text("Sandbox");
-		$("#topControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.sandbox.top();
-		});
-		
-		$("#leftText").text("The Guy");
-		$("#leftControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.me.left();
-		});
-	};
-	
-	
-	parallax.sandbox.onload=function(){
-		$("#bottomText").text("Home");
-		$("#bottomControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.index.bottom();
-		});
-	};
-	
-	parallax.boo.onload=function(){
+	//Clears each page navigation on load
+	parallax.preload = function(){
+		rightKey = leftKey = topKey = bottomKey = "";
 		$(".control").hide();
 	};
 	
 
+	//Setting up page navigation
+	parallax.index.onload=function(){
+		setRight("features", "Features");
+		setTop("sandbox", "SandBox");
+		setLeft("me","The Guy");
+	};
+	
+	parallax.sandbox.onload=function(){
+		setBottom("index","Home");
+	};
 	
 	parallax.features.onload=function(){
-		$("#leftText").text("Home");
-		$("#leftControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.index.left();
-		});
-		
-		$("#rightText").text("The Guy");
-		$("#rightControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.me.right();
-		});
+		setLeft("index", "Home");
+		setRight("me", "The Guy");
 	};
-	
 	
 	parallax.me.onload=function(){
-		$("#leftText").text("Features");
-		$("#leftControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.features.left();
-		});
-		
-		$("#rightText").text("Home");
-		$("#rightControl").show().unbind('click').click(function(){
-			$(".control").hide();
-			parallax.index.right();
-		});
+		setLeft("features", "Features");
+		setRight("index","Hone");
 	};
+	
+	
+	//Sets the correct triggers for the arrows, plus arrow keys
+	function setRight(page, text){
+		$("#rightText").text(text);
+		$("#rightControl").show().unbind('click').click(function(){
+			parallax[page].right();
+		});
+		rightKey = function(){ 
+			parallax[page].right(); 
+		};
+	}
+	
+	function setLeft(page, text){
+		$("#leftText").text(text);
+		$("#leftControl").show().unbind('click').click(function(){
+			parallax[page].left();
+		});
+		leftKey = function(){ 
+			parallax[page].left(); 
+		};
+	}
+	
+	function setTop(page, text){
+		$("#topText").text(text);
+		$("#topControl").show().unbind('click').click(function(){
+			parallax[page].top();
+		});
+		topKey = function(){ 
+			parallax[page].top(); 
+		};
+	}
+	
+	function setBottom(page, text){
+		$("#bottomText").text(text);
+		$("#bottomControl").show().unbind('click').click(function(){
+			parallax[page].bottom();
+		});
+		bottomKey = function(){ 
+			parallax[page].bottom(); 
+		};
+	}
 	
 	//The fadey bits
 	$("#bottomControl").mouseenter(function(){
@@ -86,7 +104,7 @@ $(document).ready(function () {
 		$("#bottomText").stop().fadeTo(500,0);
 	});
 	
-$("#leftControl").mouseenter(function(){
+	$("#leftControl").mouseenter(function(){
 		$("#leftArrow").fadeTo(500,1);
 		$("#leftText").fadeTo(500,1);
 	}).mouseleave(function(){
@@ -94,7 +112,7 @@ $("#leftControl").mouseenter(function(){
 		$("#leftText").stop().fadeTo(500,0);
 	});
 	
-$("#rightControl").mouseenter(function(){
+	$("#rightControl").mouseenter(function(){
 		$("#rightArrow").fadeTo(500,1);
 		$("#rightText").fadeTo(500,1);
 	}).mouseleave(function(){
@@ -102,16 +120,15 @@ $("#rightControl").mouseenter(function(){
 		$("#rightText").stop().fadeTo(500,0);
 	});
 	
-$("#topControl").mouseenter(function(){
+	$("#topControl").mouseenter(function(){
 		$("#topArrow").fadeTo(500,1);
 		$("#topText").fadeTo(500,1);
 	}).mouseleave(function(){
 		$("#topArrow").stop().fadeTo(500, 0.2);
 		$("#topText").stop().fadeTo(500,0);
 	});
-
 	
-
+	
 	$(".control").hide();
 	parallax.index.show();
 
